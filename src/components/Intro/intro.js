9efb4c492941efb69ce12b4e52cad4de19bf7f6a
kaartikeya <kaartikeya.1@gmail.com>
2024-10-15 15:28:20 +0530

    changes commit

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -10,12 +10,12 @@ export const Intro = () => {
       <div className="introContent">
         <span className="hello">Hello,</span>
         <span className="introText">
-          I'm <span className="introName">Kaartikeya Srivastava</span> <br />
+          I'm <span className="introName">Kaartikeya Srivastava,</span> <br />
           Website Developer
         </span>
         <p className="introPara">
           I am a skilled web developer with experience in creating <br />{" "}
-          reactive and scalable full stack websites. <br />
+          reactive and scalable full-stack websites. <br />
         </p>
       </div>
       <img src={bg} alt="Profile" className="bg" />
--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -2,7 +2,9 @@ import React from "react";
 import "./works.css";
 import Project1 from "../../assets/pig_game.png";
 import Project2 from "../../assets/youtube_clone.png";
-import Project3 from "../../assets/movieland.png";
+// import Project3 from "../../assets/movieland.png";
+// import Project4 from "../../assets/flappy_bird.png";
+import Project5 from "../../assets/task_list.png";
 // import Project4 from "../../assets/portfolio-4.png";
 // import Project5 from "../../assets/portfolio-5.png";
 // import Project6 from "../../assets/portfolio-6.png";
@@ -18,12 +20,18 @@ export const Works = () => {
         online presence.
       </span>
       <div className="worksImgs">
-        <img src={Project1} alt="img1" className="worksImg" />
-        <img src={Project2} alt="img2" className="worksImg" />
-        <img src={Project3} alt="img3" className="worksImg" />
-        {/* <img src={Project4} alt="img4" className="worksImg" />
-        <img src={Project5} alt="img5" className="worksImg" />
-        <img src={Project6} alt="img6" className="worksImg" /> */}
+        <a href="https://github.com/kaartikeya/Pig-game">
+          <img src={Project1} alt="img1" className="worksImg" />
+        </a>
+        <a href="https://github.com/kaartikeya/Youtube-clone">
+          <img src={Project2} alt="img2" className="worksImg" />
+        </a>
+        {/* <img src={Project3} alt="img3" className="worksImg" /> */}
+        {/* <img src={Project4} alt="img4" className="worksImg" /> */}
+        <a href="https://github.com/kaartikeya/Travel-list">
+          <img src={Project5} alt="img5" className="worksImg" />
+        </a>
+        {/* <img src={Project6} alt="img6" className="worksImg" /> */}
       </div>
       {/* <button className="worksBtn">See More</button> */}
     </section>
